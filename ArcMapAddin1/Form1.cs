8915ce04819cf30004ee31a05846c36b78929874
guojiangfu <863839052@qq.com>
2020-05-23 09:44:47 +0800

    完成基本的功能及轮廓（还未完成遍历栅格像元的功能）

--- ArcMapAddin1/Form1.cs
+++ ArcMapAddin1/Form1.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace ArcMapAddin1
     {
         IMxDocument pMxd = null;
         IMap pMap = null;
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -87,7 +88,7 @@ namespace ArcMapAddin1
             pFLayer.Name = pFC.AliasName;
             return pFLayer;
         }
-       // 获取文件的文件路径和文件名
+        // 获取文件的文件路径和文件名
         public List<string> GetFilePath()
         {
             List<string> pathList = new List<string>();
@@ -110,16 +111,16 @@ namespace ArcMapAddin1
         public List<string> SaveAsPath()
         {
             List<string> pathList = new List<string>();
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Title = "选择文件存储位置";
-            openFileDialog1.InitialDirectory = @"E:\GIS底层实验\WorkSpace";
-            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "选择文件存储位置";
+            saveFileDialog1.InitialDirectory = @"E:\GIS底层实验\WorkSpace";
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
             {
                 return null;
             }
-            string filePath = openFileDialog1.FileName;//绝对路径
-            string fileFolder = System.IO.Path.GetDirectoryName(filePath);//shp文件所在的文件夹
-            string fileName = System.IO.Path.GetFileName(filePath);//shp文件名
+            string filePath = saveFileDialog1.FileName;//绝对路径
+            string fileFolder = Path.GetDirectoryName(filePath);//文件所在的文件夹
+            string fileName = Path.GetFileName(filePath);//文件名
             pathList.Add(fileFolder);
             pathList.Add(fileName);
             pathList.Add(filePath);
@@ -140,8 +141,8 @@ namespace ArcMapAddin1
             return pFeatureClass;
         }
 
-        //获取字段名
-        public /*List<string>*/void  GetFieldsName(IFeatureClass pFeatureClass)
+        //获取字段名添加到comboBox中
+        public void GetFieldsName(IFeatureClass pFeatureClass)
         {
             IFields pFields = pFeatureClass.Fields;
             int fieldsCount = pFields.FieldCount;
@@ -149,6 +150,48 @@ namespace ArcMapAddin1
             {
                 comboBox1.Items.Add(pFields.get_Field(i).Name);
             }
+        }
+        //获取宽高中的最小值
+        //public double GetMinWH(string filePath)
+        //{
+        //    try
+        //    {
+        //        ScriptEngine pyEngine = Python.CreateEngine();//创建Python解释器对象
+        //        dynamic myPython = pyEngine.ExecuteFile(@"GetMinWH.py");//读取脚本文件
+        //        double MinWH = myPython.GetMinWH();//调用脚本文件中对应的函数
+        //        MessageBox.Show(Convert.ToString(MinWH));
+        //        return MinWH;
+        //    }
+        //    catch (SystemException ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Error");
+        //        return 0;
+        //    }
+        //}
+
+        //获取宽高中的最小值
+        public double GetMinWH(string shpfilepath)
+        {
+            double Xmin;
+            double Ymin;
+            double Xmax;
+            double Ymax;
+            List<double> WH = new List<double>();
+            //打开.shp文件,读取x,y坐标的信息
+            FileStream fs = new FileStream(shpfilepath, FileMode.Open, FileAccess.Read);   //文件流形式
+            BinaryReader BinaryFile = new BinaryReader(fs);     //打开二进制文件  
+
+            BinaryFile.ReadBytes(32);  //先读出36个字节
+            int shapetype = BinaryFile.ReadInt32();
+
+            Xmin = BinaryFile.ReadDouble();
+            Ymin = BinaryFile.ReadDouble();
+            Xmax = BinaryFile.ReadDouble();
+            Ymax = BinaryFile.ReadDouble();
+
+            WH.Add(Xmax - Xmin);
+            WH.Add(Ymax - Ymin);
+            return WH.Min();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -160,14 +203,18 @@ namespace ArcMapAddin1
             pMap.AddLayer(featureLayer);
             textBox1.Text = filePathList[2];
             GetFieldsName(featureLayer.FeatureClass);
+            comboBox1.SelectedIndex = 0;
+            textBox2.Text = Convert.ToString(GetMinWH(textBox1.Text)/250);
+            textBox3.Text = Convert.ToString(GetMinWH(textBox1.Text) / 250);
+            textBox4.Text = Convert.ToString(GetMinWH(textBox1.Text) / 30);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             string rasterSavePath = @"E:\GIS底层实验\WorkSpace\MyAddIn\result";
             string filePath = textBox1.Text;
-            string fileFolder = System.IO.Path.GetDirectoryName(filePath);//shp文件所在的文件夹
-            string fileName = System.IO.Path.GetFileName(filePath);//shp文件名
+            string fileFolder = Path.GetDirectoryName(filePath);//shp文件所在的文件夹
+            string fileName = Path.GetFileName(filePath);//shp文件名
             IWorkspaceFactory pWorkspaceFactory = new ShapefileWorkspaceFactory();
             IWorkspace pWorkspace = pWorkspaceFactory.OpenFromFile(fileFolder, 0);
             IFeatureWorkspace pFeatureWorkspace = pWorkspace as IFeatureWorkspace;
@@ -189,7 +236,7 @@ namespace ArcMapAddin1
         private void button3_Click(object sender, EventArgs e)
         {
             List<string> savePath = SaveAsPath();
-            textBox4.Text = savePath[0];
+            textBox5.Text = savePath[0];
         }
 
         private void button4_Click(object sender, EventArgs e)
